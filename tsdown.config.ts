98a95a53c0ddcefb2489e26a7406ac374702ab80
CyanSalt <c.salt@hotmail.com>
2026-06-24 11:29:29 +0800

    fix: dist extension

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts'],
   target: 'node20',
+  fixedExtension: false,
   tsconfig: false, // disable tsconfig `paths` when bundling
   dts: true,
 })
